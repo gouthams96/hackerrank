5a715f2831fd40672ba30a30e7d8939edd547553
Goutham S <gouthamsajeev1996@gmail.com>
2016-12-24 20:34:59 +0530

    Update SumvsXOR.java

--- algorithms/Bit-Manipulation/SumvsXOR.java
+++ algorithms/Bit-Manipulation/SumvsXOR.java
@@ -4,7 +4,7 @@ public class SumvsXOR {
         Scanner in = new Scanner(System.in);
         long n = in.nextLong();
          if(n>0){
-              //calculate number of zeroes
+              //calculating number of zeroes
               long x= Long.toBinaryString(n).length()-Long.bitCount(n);
               System.out.println((long)Math.pow(2,x));
            }else{
