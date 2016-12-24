91bdb545b129b48c6ff72ca34a5ff704fd13c7af
Goutham S <gouthamsajeev1996@gmail.com>
2016-12-24 20:21:34 +0530

    Create SumvsXOR.java

---
+++ algorithms/Bit-Manipulation/SumvsXOR.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SumvsXOR {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+         if(n>0){
+              long x= Long.toBinaryString(n).length()-Long.bitCount(n);
+              System.out.println((long)Math.pow(2,x));
+           }else{
+               System.out.println(1);
+           };
+    }
+}
